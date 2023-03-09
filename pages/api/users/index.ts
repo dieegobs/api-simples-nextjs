45575a13da4aa14e846f8573806dcb07a972e160
dieegobs <diegohevant@gmail.com>
2023-03-08 23:15:26 -0300

    first commit

---
+++ pages/api/users/index.ts
@@ -0,0 +1,9 @@
+import { NextApiHandler} from "next";
+import { Users } from '../../../utils/users';
+
+const handler: NextApiHandler = (req, res) => {
+    res.json(Users);
+
+}
+
+export default handler;
---
+++ utils/users.ts
@@ -0,0 +1,7 @@
+export const Users = [
+    {id: 1, name: 'Diego', age: 32},
+    {id: 2, name: 'Julia', age: 24},
+    {id: 3, name: 'Bruna', age: 34},
+    {id: 4, name: 'Luana', age: 15},
+    {id: 5, name: 'Carlos', age: 29},
+];
