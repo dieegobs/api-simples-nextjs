b9dd77d9020f320b8e2024db4be2f73a9e06b2c6
dieegobs <diegohevant@gmail.com>
2023-03-09 21:26:31 -0300

    criando get e post user

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,9 +1,32 @@
 import { NextApiHandler} from "next";
 import { Users } from '../../../utils/users';
 
-const handler: NextApiHandler = (req, res) => {
-    res.json(Users);
+const handlerGet: NextApiHandler = (req, res) => {
+    const {search, age} = req.query;
+    res.status(200).json(Users);
 
 }
+
+
+const handlerPost: NextApiHandler = async (req, res) => {
+    const {name, age} = req.body;
+
+    res.status(201).json({status: true, user: {name, age}});
+
+}
+
+
+const handler: NextApiHandler = async (req, res) => {
+   switch (req.method) {
+    case 'GET':
+        handlerGet(req, res);
+        break
+    case 'POST':
+        handlerPost(req, res);
+        break;
+   }
+
+}
+
 
 export default handler;
