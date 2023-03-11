955a577fa576958c03edd1312d74e489537a1970
dieegobs <diegohevant@gmail.com>
2023-03-10 22:10:17 -0300

    Fazendo o put do projeto

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -2,8 +2,8 @@ import {NextApiHandler} from "next";
 import { Users } from '../../../utils/users';
 import prisma from '../../../libs/prisma';
 
-
-const handler: NextApiHandler = async (req, res) => {
+// Reading user info
+const handlerGet: NextApiHandler = async (req, res) => {
       
       const { id } = req.query;
 
@@ -21,5 +21,45 @@ const handler: NextApiHandler = async (req, res) => {
       res.json({ error: 'Usuário não encontrado' });
 
 }
+
+
+const handlerPut: NextApiHandler = async (req, res) => {
+    const {name, active} = req.body;
+    const {id} = req.query;
+
+    const updatedUser = await prisma.user.update({
+      where:{
+        id: parseInt(id as string)
+      },
+      data: {
+        name, 
+        active: active === 'true' ? true : false
+      }
+    });
+
+    if(updatedUser){
+      res.json({status: true, user: updatedUser});
+      return;
+    }
+
+    res.json({error: 'não foi possível alterar este usuário'});
+
+
+}
+
+const handler: NextApiHandler = async (req, res) => {
+
+  switch(req.method){
+    case 'GET':
+      handlerGet(req, res);
+      break;
+      case 'PUT':
+      handlerPut(req, res);
+      break;
+  }
+}
+
+
+
 
 export default handler;
