981b274b0ae798ff6ea7352e9047585ebbc2ea09
dieegobs <diegohevant@gmail.com>
2023-03-12 00:48:05 -0300

    Fazendo a paginação

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -3,9 +3,34 @@ import { Users } from '../../../utils/users';
 import prisma from '../../../libs/prisma';
 
 const handlerGet: NextApiHandler = async (req, res) => {
+
+    const {page} = req.query;
+
+    //itens por página
+    let take = 4;
+
+    //offset of items
+    let skip = 0;
+    if(page){
+        skip = (parseInt(page as string) - 1) * take;
+    }
+
+
     const users = await prisma.user.findMany({
+        skip,
+        take,
+
         where: {
-            active: true
+           active: true
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            active: true,
+        },
+        orderBy: {
+            id: 'asc'
         }
     }
     );
