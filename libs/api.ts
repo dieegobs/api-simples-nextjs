5132b88feba458606bd0055cf9389079bf031c15
dieegobs <diegohevant@gmail.com>
2023-03-12 23:29:51 -0300

    Fazendo primeira parte do DRY

---
+++ libs/api.ts
@@ -0,0 +1,37 @@
+import prisma from './prisma';
+
+export default {
+    getAllUsers: async (page: number) => {
+
+        //itens por página
+        let take = 4;
+
+        //offset of items
+        let skip = 0;
+        if (page) {
+            skip = (page - 1) * take;
+        }
+
+
+        const users = await prisma.user.findMany({
+            skip,
+            take,
+
+            where: {
+                active: true
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                active: true,
+            },
+            orderBy: {
+                id: 'asc'
+            }
+        }
+        );
+
+
+    }
+}
